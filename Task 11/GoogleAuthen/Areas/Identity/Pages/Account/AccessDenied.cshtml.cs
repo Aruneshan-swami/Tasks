0827707ca27900ed85070ebf7b468ee7b3187f6b
Aruneshan-swami <aruneshan.swami@aspiresys.com>
2024-02-01 09:43:10 +0530

    MFA

---
+++ Task 11/GoogleAuthen/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GoogleAuthen.Areas.Identity.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
